83d778b1e4885c6f3279563b52c9ab54db2f3b12
Jake Holland <contact@hollandjake.com>
2025-12-13 11:09:32 +0000

    feat: Improve create performance (#22)
    * fix: correctly handle move operation when from doesn't exist
    
    - This was missed from tests but is defined in the spec under section 4.4
    
    `The "from" location MUST exist for the operation to be successful.`
    
    * chore: migrate to biome
    
    * feat: reduce patch size when a full replacement would be better
    
    - this only applies to the defaultDiffers, user-defined diff functions will not be affected

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 export { apply } from './apply';
 export { create } from './create';
-export type { Maxi, MaxiPatch, Mini, MiniPatch, Patch, Serial, SerialPatch } from './patch';
+export type {
+  Maxi,
+  MaxiPatch,
+  Mini,
+  MiniPatch,
+  Patch,
+  Serial,
+  SerialPatch,
+} from './patch';
 export { Pointer, RootPointer } from './pointer';
 export { eq, transform } from './utils';
